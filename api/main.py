017633f5583cfd3c0d0b675cdaf618bfd9c1d67a
Acelle Krislette Rosales <acellekrislette@gmail.com>
2026-04-22 23:31:45 +0800

    Allowed deployed frontend

--- api/main.py
+++ api/main.py
@@ -76,7 +76,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://kotabi.vercel.app",
+    ],
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
